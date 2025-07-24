cb2fb1a34eb50a144958749ebeb336db596df823
digvijaysingh1707 <digvijays17072003@gmail.com>
2025-07-24 16:47:58 +0530

    Minor Fix

--- pages/hrc.py
+++ pages/hrc.py
@@ -8,4 +8,4 @@ df_3m = pd.read_csv("data/hrc_pred_results_3M_till_Sep.csv")
 st.line_chart(df_3m[["ds", "actual", "horizon_0"]].set_index("ds"))
 
 st.subheader("Forecast Accuracy Metrics")
-st.dataframe(df_3m[["ds", "horizon_0_mae", "horizon_0_mape"]])
+st.dataframe(df_3m[["ds", "horizon_0", "horizon_0_mae", "horizon_0_mape"]])
